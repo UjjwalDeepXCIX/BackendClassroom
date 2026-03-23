451e0f36c1927c3696d39c9ed5b1b37b1a40ee78
Ujjwal Deep <ujjwaldeep.bincom@gmail.com>
2026-03-23 17:37:51 +0000

    added routes, cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
-
+import subjectsrouter from "./routes/subjects.js";
+import cors from "cors"
 const app = express();
 const PORT = 8000;
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
 app.use(express.json());
 
+app.use('/api/subjects', subjectsrouter)
 app.get('/', (req, res)=> {
     res.send('this is the beginning of backend api');
 });
